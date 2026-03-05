dd20dfa8b27b0d66b60162575b7df9683a3a59dc
huudung <huudung@topengnet.com>
2026-03-05 13:51:31 +0700

    PanelClodeCheck

--- SDV_MoldingInjection/MVVM/ViewModels/AutoViewModel.cs
+++ SDV_MoldingInjection/MVVM/ViewModels/AutoViewModel.cs
@@ -63,7 +63,7 @@ namespace SDV_MoldingInjection.MVVM.ViewModels
             Devices.Inputs.H4_CylDown.RaiseValueUpdated();
             Devices.Inputs.BelowsUp.RaiseValueUpdated();
             Devices.Inputs.DryPumpRun.RaiseValueUpdated();
-            Devices.Inputs.MainPanelCloseCheck.RaiseValueUpdated();
+            Devices.Inputs.PanelClodeCheck.RaiseValueUpdated();
             UpdateSyringeStatus();
         }
 
