3049ddf5f03a91fb071546fab9d156c9e268246b
Nguyễn Quang Linh <linhnq888@gmail.com>
2026-04-14 09:43:50 +0700

    Add EqpStatus

---
+++ SDV_MoldingInjection/MVVM/Views/Monitor/EquipStateStatusView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SDV_MoldingInjection.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for EquipStateStatusView.xaml
+    /// </summary>
+    public partial class EquipStateStatusView : UserControl
+    {
+        public EquipStateStatusView()
+        {
+            InitializeComponent();
+        }
+    }
+}
